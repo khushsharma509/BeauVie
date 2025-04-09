65a67473adf96831e88e534a1f2fe309442154d5
gautamnitya <124081326+gautamnitya@users.noreply.github.com>
2025-04-09 12:20:15 +0530

    WishList added using sequelize

---
+++ backend/models/wishlist.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const Wishlist = sequelize.define('Wishlist', {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+
+  return Wishlist;
+};
