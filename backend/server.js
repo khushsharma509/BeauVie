789037b4c104cb507b2b08df3fb8afa9b75e19e7
gautamnitya <124081326+gautamnitya@users.noreply.github.com>
2025-04-09 12:20:15 +0530

    WishList added using sequelize

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
 const productRouter = require('./routes/products');
 const profileRouter = require('./routes/profile');
 const cartRouter = require('./routes/cart');
 const orderRouter = require('./routes/orders');
+const wishListRouter = require('./routes/wishlist');
 
 dotenv.config(); 
 
@@ -17,10 +18,11 @@ app.use(express.json());
 
 // routes
 app.use('/api/products', productRouter);
-app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 app.use('/api/profile', profileRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/orders', orderRouter);
+app.use('/api/wishlist', wishListRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
